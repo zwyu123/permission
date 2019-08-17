8f64f309e13cb9b25668d27e9157799bc91ad46e
zwyu123 <zwyu2017@163.com>
2019-08-17 21:55:09 +0800

    部门层级树

--- src/main/java/com/mmall/dao/SysDeptMapper.java
+++ src/main/java/com/mmall/dao/SysDeptMapper.java
@@ -2,6 +2,8 @@ package com.mmall.dao;
 
 import com.mmall.model.SysDept;
 
+import java.util.List;
+
 public interface SysDeptMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,6 @@ public interface SysDeptMapper {
     int updateByPrimaryKeySelective(SysDept record);
 
     int updateByPrimaryKey(SysDept record);
+
+    List<SysDept> getAllDept();
 }
