86b576b5b647def27d622314d0c1da9af4d39d59
zwyu123 <zwyu2017@163.com>
2019-08-28 20:47:05 +0800

    新增、更新角色

---
+++ src/main/java/com/mmall/controller/SysRoleController.java
@@ -0,0 +1,66 @@
+package com.mmall.controller;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.mmall.common.JsonData;
+import com.mmall.model.SysRole;
+import com.mmall.model.SysUser;
+import com.mmall.param.RoleParam;
+import com.mmall.service.SysRoleAclService;
+import com.mmall.service.SysRoleService;
+import com.mmall.service.SysRoleUserService;
+import com.mmall.service.SysTreeService;
+import com.mmall.service.SysUserService;
+import com.mmall.util.StringUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/sys/role")
+public class SysRoleController {
+
+    @Resource
+    private SysRoleService sysRoleService;
+    @Resource
+    private SysTreeService sysTreeService;
+    @Resource
+    private SysRoleAclService sysRoleAclService;
+    @Resource
+    private SysRoleUserService sysRoleUserService;
+    @Resource
+    private SysUserService sysUserService;
+
+    @RequestMapping("role.page")
+    public ModelAndView page() {
+        return new ModelAndView("role");
+    }
+
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveRole(RoleParam param) {
+        sysRoleService.save(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateRole(RoleParam param) {
+        sysRoleService.update(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/list.json")
+    @ResponseBody
+    public JsonData list() {
+        return JsonData.success(sysRoleService.getAll());
+    }
+}
